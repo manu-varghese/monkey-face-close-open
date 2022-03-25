f970a21df867cfc21a23d147b3458f5fcff361f7
Manu Varghese <manuvarghese50@gmail.com>
2022-03-25 13:44:18 +0530

    project complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-    
+    function change(){
+      
+        let image = document.getElementById('img')
+        
+        if( image.src.match('img/eyes_open.png')){
+            console.log('eyes is open');
+            document.getElementById('img').src = "img/eyes_close.png";
+        }else{
+            console.log('eyes is close');
+            document.getElementById("img").src = "img/eyes_open.png";
+        }
+
+    }
